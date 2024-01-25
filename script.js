d42d9a81fc734f4f57caeeb15e81a2a19a08358d
ninaninon <jhonata.ozanam@gmail.com>
2024-01-25 18:46:52 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const searchInput = document.getElementById('search-input');
+const resultsArtist = document.getElementById("result-artist");
+const resultPlaylists = document.getElementById('result-playlists');
+
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+}
+
+function displayResults(result) {
+    resultPlaylists.classList.add("hidden");
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultsArtist.classList.remove('hidden');
+}
+
+
+
+document.addEventListener('input', function() {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchInput === '') {
+        resultPlaylists.classList.add('hidden');
+        resultsArtist.classList.remove('hidden');
+        return
+
+    }
+
+    requestApi(searchTerm);
+})
